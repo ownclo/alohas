a8d6f4559af4b15dbf2373c8d2f14d3155333460
Sokolovskiy Roman <rsokolovskiy@marathonbet.ru>
2015-07-07 18:22:06 +0300

    Transition to lenses, undefined state

--- Model.hs
+++ Model.hs
@@ -1,15 +1,22 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Model where
 
 import Control.Monad.State
+import Control.Applicative
+import Control.Lens
 
 data ModelState = ModelState {
-        channel :: Channel,
-        station :: Station,
-        users :: [User],
-        stats :: Stats
+        _forwChannel :: ForwChannel,
+        _backChannel :: BackChannel,
+        _station :: Station,
+        _users :: [User],
+        _stats :: Stats
     } deriving Show
 
-data Channel = Channel
+data ForwChannel = ForwChannel
+  deriving Show
+
+data BackChannel = BackChannel
   deriving Show
 
 data Station = Station
@@ -21,21 +28,51 @@ data User = User
 data Stats = Stats
   deriving Show
 
-type Model = State ModelState ()
+data ForwMsg = ForwMsg
+  deriving Show
+
+makeLenses ''ModelState
+
+type Model = State ModelState
 
 initModel :: ModelState
 initModel = ModelState {
-        channel = Channel,
-        station = Station,
-        users = [],
-        stats = Stats
+        _forwChannel = ForwChannel,
+        _backChannel = BackChannel,
+        _station = Station,
+        _users = [],
+        _stats = Stats
     }
 
-runModel :: Model -> ModelState -> ModelState
-runModel = execState
+runModel :: Int -> ModelState
+runModel num = execState steps initModel
+    where steps = replicateM_ num stepModel
 
-stepModel :: Model -> Model
-stepModel = undefined
+stepModel :: Model ()
+stepModel = do
+    msgs <- stepUsersBefore
+    stepForwChannel
+    stepStation
+    stepBackChannel
+    stepUsersAfter
+
+stepUsersBefore :: Model [ForwMsg]
+stepUsersBefore = zoom (users.traversed) stepUserBefore
+
+stepUserBefore :: State User [ForwMsg]
+stepUserBefore = return [ForwMsg]
+
+stepForwChannel :: Model ()
+stepForwChannel = undefined
+
+stepStation :: Model ()
+stepStation = undefined
+
+stepBackChannel :: Model ()
+stepBackChannel = undefined
+
+stepUsersAfter :: Model ()
+stepUsersAfter = undefined
 
 main :: IO ()
-main = print $ flip runModel initModel $ return ()
+main = print $ runModel 1
